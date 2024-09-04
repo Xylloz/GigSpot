f55cefcedefeb188fd3cf578a7334985a91f2ea5
Richard Gullo <tehxyllo@gmail.com>
2024-09-04 10:36:47 -0400

    feat: added Mongoose userModel, jobModel

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import Splashpage from '../components/splashpage.tsx';
+import SplashPage from '../components/SplashPage.tsx';
 import UserForm from '../components/UserForm.tsx';
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')!).render(
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/splashpage" element={<Splashpage />} />
+        <Route path="/splashpage" element={<SplashPage />} />
         <Route path="/employee" element={<UserForm formType="employee" />} /> 
         <Route path="/employer" element={<UserForm formType="employer" />} />  
       </Routes>
