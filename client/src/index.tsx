a43c5222be37ec1f5d957d6634a34d4dfa262d29
SameerSyed99 <ssyed1999@outlook.com>
2024-09-03 22:21:01 -0400

    work

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import Splashpage from '../components/splashpage.tsx';
+import UserForm from '../components/UserForm.tsx';
+import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/splashpage" element={<Splashpage />} />
+        <Route path="/employee" element={<UserForm formType="employee" />} /> 
+        <Route path="/employer" element={<UserForm formType="employer" />} />  
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+);
